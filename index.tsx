6bda2b0d8becd8003eb35d34afeeabfc93d64f2e
eps1lon <silbermann.sebastian@gmail.com>
2021-03-19 14:26:41 +0100

    Initial commit

---
+++ index.tsx
@@ -0,0 +1,13 @@
+/* eslint-disable react/react-in-jsx-scope -- Unaware of jsxImportSource */
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
+
+export default function Component() {
+  return (
+    <input
+      css={css`
+        color: red;
+      `}
+    />
+  );
+}
